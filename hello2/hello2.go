99e209f07b23bc9cac71554f8d5fcc52f3a84d37
kongbong sik <kongbong@kongbongui-Mac-mini.local>
2016-04-14 21:56:43 +0900

    add others

---
+++ hello2/hello2.go
@@ -0,0 +1,7 @@
+package hello2
+
+import "fmt"
+
+func hello2() {
+    fmt.Println("Hello world")    
+}
---
+++ hello2/hello2_test.go
@@ -0,0 +1,7 @@
+package hello2
+
+import "testing"
+
+func TestHello2(t *testing.T) {
+    hello2()
+}
